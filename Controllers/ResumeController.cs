1e71d542fa5ebd83a1414a29e106fa522d3ca4fe
jesperbertling <jesper.bertling@hotmail.com>
2023-12-18 12:58:09 +0100

    CV

---
+++ Controllers/ResumeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkillSkulptor.Models;
+
+namespace SkillSkulptor.Controllers
+{
+    public class ResumeController : Controller 
+        
+    {
+        private readonly ILogger<ResumeController> _logger;
+        private SsDbContext _dbContext;
+
+        public ResumeController(ILogger<ResumeController> logger, SsDbContext _db)
+        {
+            _dbContext = _db;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            AppUser myUser = _dbContext.AppUsers.First();
+            return View(myUser);
+        }
+    }
+}
