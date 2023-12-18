237c9e9b393752897da7e0545a516bdee983359f
Luddland <120645933+Luddland@users.noreply.github.com>
2023-12-18 13:03:09 +0100

    Nyview+controller för "Alla Projekt"

---
+++ Controllers/ProjketController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SkillSkulptor.Models;
+
+namespace SkillSkulptor.Controllers
+{
+	public class ProjketController : Controller
+	{
+		private SsDbContext _dbContext;
+
+
+		public ProjketController(SsDbContext _db)
+		{
+			_dbContext = _db;
+		}
+
+
+		[HttpGet]
+		public IActionResult Projekt()
+		{
+			List<Project> _projects = new List<Project>();
+			return View(_projects);
+		}
+	}
+
+}
