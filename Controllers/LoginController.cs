726712e9f7952029ac9b88a9ce0c119e479cd091
MBaven <Martin.baven@telia.com>
2023-12-18 13:05:09 +0100

    LoginController

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using SkillSkulptor.Models;
+
+namespace SkillSkulptor.Controllers
+{
+	public class LoginController : Controller
+	{
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		public ActionResult Signup()
+		{
+			return View();
+		}
+
+		public ActionResult Signout()
+		{
+			return View();
+		}
+	}
+}
