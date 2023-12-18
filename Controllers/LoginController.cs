e41423865585e1fb4e6c6d41fe786225aeb67d72
MBaven <Martin.baven@telia.com>
2023-12-18 14:29:19 +0100

    La in i viewen

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -2,24 +2,26 @@
 using System;
 using System.Linq;
 using SkillSkulptor.Models;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Identity;
 
 namespace SkillSkulptor.Controllers
 {
 	public class LoginController : Controller
 	{
+
 		public ActionResult Login()
 		{
 			return View();
 		}
-
 		public ActionResult Signup()
 		{
 			return View();
 		}
-
 		public ActionResult Signout()
 		{
 			return View();
 		}
+	
 	}
 }
