ef938983af5307e3f85ed81cb3aaf2f07bc834db
gotesjo <120645559+gotesjo@users.noreply.github.com>
2023-12-19 11:23:03 +0100

    fic

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkillSkulptor.Models;
+
+namespace SkillSkulptor.Controllers
+{
+    public class MessageController : Controller
+    {
+        private SsDbContext _dbContext;
+
+        public MessageController(SsDbContext _db)
+        {
+            _dbContext = _db;
+        }
+
+
+		public IActionResult Index()
+		{
+			AppUser _loggedInUser = GetLoggedInUser(); 
+
+			List<AppUser> _users = _dbContext.AppUsers.ToList();
+
+			var viewModel = new SearchViewModel
+			{
+				LoggedInUser = _loggedInUser,
+				Users = _users
+			};
+
+			return View(viewModel);
+		}
+
+		[HttpPost]
+		public IActionResult Search(string searchString)
+		{
+			if (string.IsNullOrEmpty(searchString))
+			{
+				return RedirectToAction("Index");
+			}
+
+			AppUser _loggedInUser = GetLoggedInUser(); 
+
+			List<AppUser> _users = _dbContext.AppUsers.Where(user => user.Firstname.Contains(searchString)).ToList();
+
+			var viewModel = new SearchViewModel
+			{
+				LoggedInUser = _loggedInUser,
+				Users = _users,
+				SearchString = searchString
+			};
+
+			return View("Index", viewModel);
+		}
+
+		private AppUser GetLoggedInUser()
+		{
+			return _dbContext.AppUsers.First();
+		}
+	}
+}
---
+++ Models/SearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillSkulptor.Models
+{
+	public class SearchViewModel
+	{
+		public string SearchString { get; set; }
+		public AppUser LoggedInUser { get; set; }
+		public List<AppUser> Users { get; set; }
+
+	}
+}
