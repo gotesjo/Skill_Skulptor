fc7dc0f81a3f7476c87914aa716d9e5c14ed9d1f
jesperbertling <jesper.bertling@hotmail.com>
2024-01-02 23:22:43 +0100

    Skapa nytt cv men inte full funktionalitet

--- Controllers/ResumeController.cs
+++ Controllers/ResumeController.cs
@@ -1,57 +1,99 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using SkillSkulptor.Models;
 
 namespace SkillSkulptor.Controllers
 {
-    public class ResumeController : Controller 
-        
+    public class ResumeController : Controller
     {
         private readonly ILogger<ResumeController> _logger;
         private SsDbContext _dbContext;
-		private UserManager<AppUser> userManager;
-		private IWebHostEnvironment _hostingEnvironment;
+        private UserManager<AppUser> userManager;
 
-        public ResumeController(ILogger<ResumeController> logger, SsDbContext _db, IWebHostEnvironment hostingEnvironment, UserManager<AppUser> _userManager)
+        public ResumeController(ILogger<ResumeController> logger, SsDbContext db, UserManager<AppUser> userManager)
         {
-            _dbContext = _db;
+            _dbContext = db;
             _logger = logger;
-            userManager = _userManager;
-			_hostingEnvironment = hostingEnvironment;
+            this.userManager = userManager;
         }
+
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-			AppUser loggedInUser = userManager.GetUserAsync(User).Result;
+            var loggedInUser = await userManager.GetUserAsync(User);
+            if (loggedInUser == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
 
-			ViewBag.defaultPicturePath = "~/datafiles/pictures/defaultpicture.png";
-            return View(loggedInUser);
+            var userCV = _dbContext.CVs.FirstOrDefault(cv => cv.BelongsTo == loggedInUser.Id);
+
+            var viewModel = new ResumeViewModel
+            {
+                User = loggedInUser,
+                UserCV = userCV
+            };
+
+            return View(viewModel);
         }
-        
-        
-        //[HttpGet("{id:int}")]
-        //public IActionResult Index(int id)
-        //{
-        //  var myUser = _dbContext.Users.FirstOrDefault(u => u.UserId == id);
-        //        return View (myUser);
-        //}
-        //public async Task<IActionResult> UserImage(int userId)
-        //{
-        //    var userPicture = await _dbContext.ProfilePictures
-        //                                      .FirstOrDefaultAsync(p => p.pictureUser.UserId == userId);
-
-        //    if (userPicture?.ImageData != null)
-        //    {
-        //        // Anta att bilden är av typen JPEG, uppdatera MIME-typen enligt bildformatet
-        //        return File(userPicture.ImageData, "image/jpeg");
-        //    }
 
 
-        //    var path = Path.Combine(_hostingEnvironment.WebRootPath, "images", "default-profile.png");
-        //    var imageBytes = await System.IO.File.ReadAllBytesAsync(path);
-        //    return File(imageBytes, "image/png"); // Anpassa MIME-typen enligt standardbilden
-        //}
+        [HttpGet]
+        public IActionResult CreateCV()
+        {
+            // Visa sidan för att skapa ett nytt CV
+            return View(new CV());
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateCV(CV cvModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var loggedInUser = await userManager.GetUserAsync(User);
+                if (loggedInUser != null)
+                {
+                    cvModel.BelongsTo = loggedInUser.Id;
+                    _dbContext.CVs.Add(cvModel);
+                    await _dbContext.SaveChangesAsync();
+
+                    return RedirectToAction("Index"); // Återgå till översiktssidan efter att CV:t har skapats
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Ingen inloggad användare hittades.");
+                }
+            }
+            return View(cvModel); // Om valideringsfel finns, visa formuläret igen
+        }
+
+        // Eventuella ytterligare metoder och logik...
     }
 }
+
+
+
+
+//[HttpGet("{id:int}")]
+//public IActionResult Index(int id)
+//{
+//  var myUser = _dbContext.Users.FirstOrDefault(u => u.UserId == id);
+//        return View (myUser);
+//}
+//public async Task<IActionResult> UserImage(int userId)
+//{
+//    var userPicture = await _dbContext.ProfilePictures
+//                                      .FirstOrDefaultAsync(p => p.pictureUser.UserId == userId);
+
+//    if (userPicture?.ImageData != null)
+//    {
+//        // Anta att bilden är av typen JPEG, uppdatera MIME-typen enligt bildformatet
+//        return File(userPicture.ImageData, "image/jpeg");
+//    }
+
+
+//    var path = Path.Combine(_hostingEnvironment.WebRootPath, "images", "default-profile.png");
+//    var imageBytes = await System.IO.File.ReadAllBytesAsync(path);
+//    return File(imageBytes, "image/png"); // Anpassa MIME-typen enligt standardbilden
+//}
+
---
+++ Models/ResumeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SkillSkulptor.Models
+{
+    public class ResumeViewModel
+    {
+        public AppUser User { get; set; }
+        public CV UserCV { get; set; }
+    }
+}
