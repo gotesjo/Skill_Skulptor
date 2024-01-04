93afe3d55944c21ebcd651495e521e3150781a18
axeRob <121172174+axeRob@users.noreply.github.com>
2024-01-04 20:36:36 +0100

    ändra cv inte full funktionallitet

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SkillSkulptor.Models;
 using System.Linq;
+using System.Security.Claims;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SkillSkulptor.Controllers
@@ -36,6 +37,23 @@ namespace SkillSkulptor.Controllers
 			return View(user);
 		}
 
+        [HttpGet]
+        public IActionResult EditCV()
+        {
+            AppUser user = userManager.GetUserAsync(User).Result;
+
+            // Skapa en ResumeViewModel-instans
+            var resumeViewModel = new ResumeViewModel
+            {
+                // Antag att ResumeViewModel har en egenskap för att lagra användarinformation
+                User = user,
+                UserCV = user.userCV // Eller hur du nu hämtar användarens CV
+            };
+
+            return View(resumeViewModel);
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> ProfileEdit(AppUser user, IFormFile file)
         {
@@ -44,6 +62,12 @@ namespace SkillSkulptor.Controllers
                 // Hämta den befintliga användaren från databasen
                 var existingUser = _dbContext.Users.FirstOrDefault(u => u.Id == user.Id);
 
+                if (existingUser == null)
+                {
+                    // Hantera fallet då användaren inte hittades
+                    return NotFound(); // eller någon annan lämplig åtgärd
+                }
+
                 // Uppdatera övriga egenskaper (förnamn, efternamn, e-post etc.)
                 existingUser.Firstname = user.Firstname;
                 existingUser.Lastname = user.Lastname;
@@ -51,16 +75,31 @@ namespace SkillSkulptor.Controllers
                 existingUser.Phonenr = user.Phonenr;
                 existingUser.Active = user.Active;
                 existingUser.ProfileAccess = user.ProfileAccess;
-                existingUser.fkAddress.Street = user.fkAddress.Street;
-                existingUser.fkAddress.City = user.fkAddress.City;
-                existingUser.fkAddress.ZipCode = user.fkAddress.ZipCode;
-                existingUser.fkAddress.Country = user.fkAddress.Country;
 
-                // Kontrollera om ett nytt lösenord har angetts
-                if (!string.IsNullOrEmpty(user.PasswordHash))
+                if (user.fkAddress != null)
                 {
-                    // Om ett nytt lösenord har angetts, uppdatera lösenordet
-                    existingUser.PasswordHash = user.PasswordHash;
+                    if (existingUser.fkAddress == null)
+                    {
+                        existingUser.fkAddress = new Adress
+                        {
+                            Street = user.fkAddress?.Street ?? "",
+                            City = user.fkAddress?.City ?? "",
+                            ZipCode = user.fkAddress?.ZipCode ?? "",
+                            Country = user.fkAddress?.Country ?? ""
+                        };
+                    }
+
+
+
+                    _dbContext.Adresses.Add(existingUser.fkAddress); // Lägg till den nya adressen i databasen
+                }
+                else
+                {
+                    existingUser.fkAddress.Street = user.fkAddress.Street;
+                    existingUser.fkAddress.City = user.fkAddress.City;
+                    existingUser.fkAddress.ZipCode = user.fkAddress.ZipCode;
+                    existingUser.fkAddress.Country = user.fkAddress.Country;
+                
                 }
 
                 if (file != null && file.Length > 0)
@@ -69,34 +108,28 @@ namespace SkillSkulptor.Controllers
                     {
                         await file.CopyToAsync(stream);
 
-                        if(existingUser.fkPicture != null)
+                        if (existingUser.fkPicture == null)
                         {
-                            existingUser.fkPicture.ImageData = stream.ToArray();
-                            existingUser.fkPicture.Filename = file.FileName;
-                        }
-                        else
-                        {
-                            existingUser.fkPicture = new Profilepicture
-                            {
-                                Filename = file.FileName,
-                                ImageData = stream.ToArray()
-                            };
+                            existingUser.fkPicture = new Profilepicture(); // Ersätt 'Profilepicture' med korrekt klassnamn
                         }
 
+                        existingUser.fkPicture.ImageData = stream.ToArray();
+                        existingUser.fkPicture.Filename = file.FileName;
                     }
                 }
 
                 _dbContext.Update(existingUser);
-                _dbContext.SaveChanges();
+                await _dbContext.SaveChangesAsync();
 
                 return RedirectToAction("Index");
-
             }
             catch (Exception ex)
             {
-                return RedirectToAction("Index", "Error"); // Visa en felvy
+
+                return RedirectToAction("Index", "Error");
             }
         }
+
 
         public async Task<IActionResult> UserImage(string userId)
         {
@@ -105,7 +138,7 @@ namespace SkillSkulptor.Controllers
 
             if (userPicture?.ImageData != null)
             {
-                // Anta att bilden är av typen JPEG, uppdatera MIME-typen enligt bildformatet
+                // Typen JPEG, uppdatera MIME-typen enligt bildformatet
                 return File(userPicture.ImageData, "image/jpeg");
             }
 
@@ -119,6 +152,109 @@ namespace SkillSkulptor.Controllers
             return File(imageBytes, mimeType);
 
         }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCV(ResumeViewModel model)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Unauthorized();
+            }
+
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var existingUser = _dbContext.Users
+                                         .Include(u => u.userCV)
+                                             .ThenInclude(cv => cv.Experiences)
+                                         .Include(u => u.userCV)
+                                             .ThenInclude(cv => cv.Educations)
+                                         .Include(u => u.userCV)
+                                             .ThenInclude(cv => cv.Qualifications)
+                                         .FirstOrDefault(u => u.Id == userId);
+
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            // Uppdatera CV-information
+            var userCV = existingUser.userCV ?? new CV();
+            userCV.Summary = model.UserCV.Summary;
+            userCV.PersonalLetter = model.UserCV.PersonalLetter;
+
+            if (model.UserCV.Experiences != null)
+            {
+                foreach (var experience in _dbContext.Experiences)
+                {
+                    var existingExperience = userCV.Experiences.FirstOrDefault(e => e.ExId == experience.ExId);
+                    if (existingExperience != null)
+                    {
+                        // Uppdatera befintlig erfarenhet med nya värden
+                        existingExperience.Position = experience.Position;
+                        existingExperience.Employer = experience.Employer;
+                        existingExperience.Description = experience.Description;
+                        existingExperience.StartDate = experience.StartDate;
+                        existingExperience.EndDate = experience.EndDate;
+
+                        _dbContext.Experiences.Update(existingExperience);
+                    }
+                }
+            }
+
+
+
+            // Uppdatera utbildningar
+            UpdateEducations(userCV, model.Educations);
+
+            // Uppdatera kvalifikationer
+            UpdateQualifications(userCV, model.Qualifications);
+
+            _dbContext.Update(existingUser);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("EditCV");
+        }
+
+        private async Task UpdateExperiences(CV userCV, List<Experience> experiences)
+        {
+            foreach (var experience in experiences)
+            {
+                var existingExperience = userCV.Experiences.FirstOrDefault(e => e.ExId == experience.ExId);
+                if (existingExperience != null)
+                {
+                    // Uppdatera befintlig erfarenhet med nya värden
+                    existingExperience.Position = experience.Position;
+                    existingExperience.Employer = experience.Employer;
+                    existingExperience.Description = experience.Description;
+                    existingExperience.StartDate = experience.StartDate;
+                    existingExperience.EndDate = experience.EndDate;
+
+                    _dbContext.Experiences.Update(existingExperience);
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
+        }
+
+
+        private void UpdateEducations(CV userCV, List<Education> educations)
+        {
+            foreach (var education in educations)
+            {
+                var existingEducation = userCV.Educations.FirstOrDefault(e => e.EdID == education.EdID);
+                _dbContext.Entry(existingEducation).CurrentValues.SetValues(education);
+            }
+        }
+
+        private void UpdateQualifications(CV userCV, List<Qualification> qualifications)
+        {
+            foreach (var qualification in qualifications)
+            {
+                var existingQualification = userCV.Qualifications.FirstOrDefault(q => q.QID == qualification.QID);
+                _dbContext.Entry(existingQualification).CurrentValues.SetValues(qualification);
+            }
+        }
+
+
+
 
 
     }
--- Models/Adress.cs
+++ Models/Adress.cs
@@ -6,9 +6,9 @@ namespace SkillSkulptor.Models
     {
         [Key]
         public int AdressID { get; set; }
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string ZipCode { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? ZipCode { get; set; }
         public string? Country { get; set; }
         public virtual AppUser resident { get; set; }
 
